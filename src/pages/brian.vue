e3171f26fd4a5cbd74039ac6706e577b575a7336
bchoows <bchoo.ws@gmail.com>
2025-05-28 11:14:10 +0800

    I've created my v-card
    git push origin feature/brian
    clear
    end

---
+++ src/pages/brian.vue
@@ -0,0 +1,10 @@
+<v-row class="mt-3" align="center">
+    <v-col cols="4">
+      <v-card class="elevation-3 rounded-lg">
+        <v-card-title>Brian</v-card-title>
+        <v-card-text
+          >Please help me. I want to die.</v-card-text
+        >
+      </v-card>
+    </v-col>
+  </v-row>
