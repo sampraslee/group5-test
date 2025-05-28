ef2af84c36ead07289b1889134d908a79c768f0c
bchoows <bchoo.ws@gmail.com>
2025-05-28 14:07:00 +0800

    I've created my v-card

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,5 +19,6 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/brian': RouteRecordInfo<'/brian', '/brian', Record<never, never>, Record<never, never>>,
   }
 }
