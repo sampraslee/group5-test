6d1803dd894a0b5cf03cf677589453d9b7d5b1b3
Sritharan0612 <152245504+Sritharan0612@users.noreply.github.com>
2025-05-29 13:46:05 +0800

    I'm done with the project

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,6 +11,17 @@
           >
         </v-card>
       </v-col>
+        <v-card>
+        <v-col cols="6">
+        <v-card class="elevation-1 rounded-lg">
+          <v-card-title>Sritharan, without the v card</v-card-title>
+          <v-card-text
+            >Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Similique, doloribus?</v-card-text
+          >
+        </v-card>
+      </v-col>
+        </v-card>
       <!-- Insert a new column here containing your v-card -->
     </v-row>
   </v-container>
